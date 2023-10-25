6dadc90f0bf53da98bbd0c5aaf402b843cfc5425
Stephen Gutekanst <stephen@hexops.com>
2023-10-24 23:45:47 -0700

    fix test target
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -35,7 +35,7 @@ pub fn build(b: *std.Build) !void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/app.zig" },
         .target = target,
         .optimize = optimize,
     });
